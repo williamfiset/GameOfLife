a004b3cf71b5c385a53c3d9dbc6e9e7d86ae3388
william-fiset <fisetwill@gmail.com>
2014-06-14 17:06:22 -0300

    First Push
    Created The Project Template

---
+++ GameOfLife/GameOfLife/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  GameOfLife
+//
+//  Created by William Fiset on 2014-06-14.
+//  Copyright (c) 2014 William Fiset. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    /* Setup your scene here */
+    override func didMoveToView(view: SKView) {
+        
+        
+        
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+            
+        }
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
