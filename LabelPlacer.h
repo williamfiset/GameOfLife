284818163be5ad8edc6f8c2d1e3763752cb65a05
william-fiset <fisetwill@gmail.com>
2014-06-14 22:53:50 -0300

    test

---
+++ LabelPlacer.h
@@ -0,0 +1,15 @@
+//
+//  LabelPlacer.h
+//  GameOfLife
+//
+//  Created by William Fiset on 2014-06-14.
+//  Copyright (c) 2014 William Fiset. All rights reserved.
+//
+
+@import UIKit;
+@import Foundation;
+#import <Foundation/Foundation.h>
+
+@interface LabelPlacer : NSObject
++ (void) placeLabelsOnView: (UIView*) view ;
+@end
