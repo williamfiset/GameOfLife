55079e85dba5be1d194393a1a872e663eec95478
micahandwill <wafiset@mta.ca>
2014-07-13 16:51:18 -0400

    Added Icon

--- GameOfLife/GameOfLife2/experimentFile.playground/section-1.swift
+++ GameOfLife/GameOfLife2/experimentFile.playground/section-1.swift
@@ -1,23 +1,6 @@
 // Playground - noun: a place where people can play
 
-import CoreGraphics
 import Foundation
-
-class Shoe {}
-
-var numbers : Int[][] = []
-
-
-for n in 0...10 {
-    
-    var tempArray : Array<Int> = []
-    
-    for n2 in 0...10 {
-        tempArray.append(n2)
-    }
-    numbers.append(tempArray)
-    println("\(numbers[0][0])")
-}
 
 
 
